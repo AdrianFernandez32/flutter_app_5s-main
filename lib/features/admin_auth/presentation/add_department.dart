7368d099f741a80a5afe3c0c39839da9a50e5291
Osminalx <verdestro64@gmail.com>
2025-03-31 18:25:46 -0700

    :sparkles: Add departments page and department Item

--- lib/features/admin_auth/presentation/add_department.dart
+++ lib/features/admin_auth/presentation/add_department.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_app_5s/features/user_auth/presentation/widgets/departmentItem.dart';
 
 class AddDepartment extends StatelessWidget {
   const AddDepartment({super.key});
+  final List<Map<String, dynamic>> mockDepartments = const [
+    {
+      'title': 'Ventas',
+      'id': '1',
+    },
+    {
+      'title': 'Recursos Humanos',
+      'id': '2',
+    },
+    {
+      'title': 'Tecnología',
+      'id': '3',
+    },
+    {
+      'title': 'Marketing',
+      'id': '4',
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -10,16 +29,28 @@ class AddDepartment extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "Agregar departamentos",
-          style: TextStyle(color: colorScheme.onPrimary),
+          "Departamentos",
+          style: TextStyle(color: colorScheme.onSurface),
         ),
-        backgroundColor: colorScheme.primary,
+        backgroundColor: colorScheme.surface,
+        centerTitle: true,
       ),
       body: Container(
           color: colorScheme.surface,
-          child: Column(
-            children: [],
+          child: ListView(
+            children: [
+              ...mockDepartments
+                  .map((department) => DepartmentItem(
+                        title: department['title'],
+                        onTap: () => _handleDepartmentTap(department['id']),
+                      ))
+                  .toList()
+            ],
           )),
     );
   }
+}
+
+void _handleDepartmentTap(String id) {
+  print('Department tapped ID: $id');
 }
