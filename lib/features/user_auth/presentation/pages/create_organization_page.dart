7e5934effe2b618e05a8ad54819056ba8450035a
AdrianFernandez32 <oscar.adrian.fersa@gmail.com>
2025-04-10 16:16:50 -0700

    updated and added logic to save org

--- lib/features/user_auth/presentation/pages/create_organization_page.dart
+++ lib/features/user_auth/presentation/pages/create_organization_page.dart
@@ -1,32 +1,89 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_app_5s/utils/color_scheme_manager.dart';
 import 'package:go_router/go_router.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
 
 class CreateOrganizationPage extends StatefulWidget {
   const CreateOrganizationPage({Key? key}) : super(key: key);
 
   @override
-  // ignore: library_private_types_in_public_api
   _CreateOrganizationPageState createState() => _CreateOrganizationPageState();
 }
 
 class _CreateOrganizationPageState extends State<CreateOrganizationPage> {
   File? _image;
   PaletteType selectedPalette = PaletteType.blue;
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  bool _isLoading = false;
 
   Future<void> _pickImage() async {
-    final ImagePicker picker = ImagePicker();
-    final XFile? pickedFile =
-        await picker.pickImage(source: ImageSource.gallery);
-
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
     if (pickedFile != null) {
-      setState(() {
-        _image = File(pickedFile.path);
-      });
+      setState(() => _image = File(pickedFile.path));
     }
+  }
+
+  Future<void> _createOrganization() async {
+    if (_nameController.text.isEmpty || _descriptionController.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Por favor completa todos los campos')),
+      );
+      return;
+    }
+
+    setState(() => _isLoading = true);
+
+    try {
+      final uri =
+          Uri.parse('https://tu-api.com/organizations'); // cambia esta URL
+      final request = http.MultipartRequest('POST', uri);
+
+      request.fields['name'] = _nameController.text;
+      request.fields['description'] = _descriptionController.text;
+      request.fields['colorPalette'] = selectedPalette.name;
+
+      if (_image != null) {
+        request.files.add(
+          await http.MultipartFile.fromPath('logo', _image!.path),
+        );
+      }
+
+      final response = await request.send();
+      final body = await response.stream.bytesToString();
+
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        if (!mounted) return;
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Organización creada exitosamente')),
+        );
+        context.goNamed('Menu');
+      } else {
+        if (!mounted) return;
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error: ${response.statusCode} - $body')),
+        );
+      }
+    } catch (e) {
+      if (!mounted) return;
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error de red: $e')),
+      );
+    } finally {
+      if (mounted) {
+        setState(() => _isLoading = false);
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _descriptionController.dispose();
+    super.dispose();
   }
 
   @override
@@ -34,170 +91,184 @@ class _CreateOrganizationPageState extends State<CreateOrganizationPage> {
     final colorScheme = Theme.of(context).colorScheme;
 
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Spacer(flex: 2),
-            Expanded(
-              flex: 3,
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: AbsorbPointer(
+              absorbing: _isLoading,
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  GestureDetector(
-                    onTap: _pickImage,
-                    child: CircleAvatar(
-                      radius: 95,
-                      backgroundColor: Colors.blue,
-                      child: CircleAvatar(
-                        radius: 92,
-                        backgroundColor: Colors.grey[200],
-                        backgroundImage:
-                            _image != null ? FileImage(_image!) : null,
-                        child: _image == null
-                            ? const Icon(Icons.add,
-                                size: 50, color: Colors.blue)
-                            : null,
-                      ),
+                  const Spacer(flex: 2),
+                  Expanded(
+                    flex: 3,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        GestureDetector(
+                          onTap: _pickImage,
+                          child: CircleAvatar(
+                            radius: 95,
+                            backgroundColor: Colors.blue,
+                            child: CircleAvatar(
+                              radius: 92,
+                              backgroundColor: Colors.grey[200],
+                              backgroundImage:
+                                  _image != null ? FileImage(_image!) : null,
+                              child: _image == null
+                                  ? const Icon(Icons.add,
+                                      size: 50, color: Colors.blue)
+                                  : null,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 15),
+                        Text(
+                          _image == null ? "Agregar foto" : "Cambiar foto",
+                          style: const TextStyle(color: Colors.blue),
+                        ),
+                      ],
                     ),
                   ),
-                  const SizedBox(height: 15),
-                  Text(
-                    _image == null ? "Agregar foto" : "Cambiar foto",
-                    style: const TextStyle(color: Colors.blue),
+                  Expanded(
+                    flex: 3,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextField(
+                          controller: _nameController,
+                          decoration: InputDecoration(
+                            hintText: "Agregar nombre de organización",
+                            hintStyle: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey,
+                            ),
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: const BorderSide(
+                                  color: Colors.grey, width: 1),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: const BorderSide(
+                                  color: Colors.grey, width: 1),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: const BorderSide(
+                                  color: Colors.blue, width: 2),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        DropdownMenu<PaletteType>(
+                          width: 400,
+                          initialSelection: selectedPalette,
+                          onSelected: (PaletteType? newValue) {
+                            if (newValue != null) {
+                              setState(() => selectedPalette = newValue);
+                            }
+                          },
+                          dropdownMenuEntries: const [
+                            DropdownMenuEntry(
+                                value: PaletteType.blue, label: "Blue"),
+                            DropdownMenuEntry(
+                                value: PaletteType.purple, label: "Purple"),
+                            DropdownMenuEntry(
+                                value: PaletteType.green, label: "Green"),
+                            DropdownMenuEntry(
+                                value: PaletteType.wine, label: "Wine"),
+                            DropdownMenuEntry(
+                                value: PaletteType.blue, label: "Default"),
+                          ],
+                          inputDecorationTheme: InputDecorationTheme(
+                            hintStyle: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey,
+                            ),
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: const BorderSide(
+                                  color: Colors.grey, width: 1),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: const BorderSide(
+                                  color: Colors.grey, width: 1),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: const BorderSide(
+                                  color: Colors.blue, width: 2),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        TextField(
+                          controller: _descriptionController,
+                          maxLines: 3,
+                          decoration: InputDecoration(
+                            hintText: "Añade una descripción",
+                            hintStyle: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey,
+                            ),
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: const BorderSide(
+                                  color: Colors.grey, width: 1),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: const BorderSide(
+                                  color: Colors.grey, width: 1),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: const BorderSide(
+                                  color: Colors.blue, width: 2),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
+                  const SizedBox(height: 30),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: colorScheme.primary,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        padding: const EdgeInsets.symmetric(vertical: 15),
+                      ),
+                      onPressed: _createOrganization,
+                      child: const Text("Crear",
+                          style: TextStyle(fontSize: 16, color: Colors.white)),
+                    ),
+                  ),
+                  const SizedBox(height: 100),
                 ],
               ),
             ),
-            Expanded(
-              flex: 3,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: "Agregar nombre de organización",
-                      hintStyle: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.grey,
-                      ),
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide:
-                            const BorderSide(color: Colors.grey, width: 1),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide:
-                            const BorderSide(color: Colors.grey, width: 1),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide:
-                            const BorderSide(color: Colors.blue, width: 2),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  DropdownMenu<PaletteType>(
-                    width: 400,
-                    initialSelection: selectedPalette,
-                    onSelected: (PaletteType? newValue) {
-                      if (newValue != null) {
-                        setState(() {
-                          selectedPalette = newValue;
-                        });
-                      }
-                    },
-                    dropdownMenuEntries: const [
-                      DropdownMenuEntry(value: PaletteType.blue, label: "Blue"),
-                      DropdownMenuEntry(
-                          value: PaletteType.purple, label: "Purple"),
-                      DropdownMenuEntry(
-                          value: PaletteType.green, label: "Green"),
-                      DropdownMenuEntry(value: PaletteType.wine, label: "Wine"),
-                      DropdownMenuEntry(
-                          value: PaletteType.blue, label: "Default"),
-                    ],
-                    inputDecorationTheme: InputDecorationTheme(
-                      hintStyle: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.grey,
-                      ),
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide:
-                            const BorderSide(color: Colors.grey, width: 1),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide:
-                            const BorderSide(color: Colors.grey, width: 1),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide:
-                            const BorderSide(color: Colors.blue, width: 2),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  TextField(
-                    maxLines: 3,
-                    decoration: InputDecoration(
-                      hintText: "Añade una descripción",
-                      hintStyle: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.grey,
-                      ),
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide:
-                            const BorderSide(color: Colors.grey, width: 1),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide:
-                            const BorderSide(color: Colors.grey, width: 1),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide:
-                            const BorderSide(color: Colors.blue, width: 2),
-                      ),
-                    ),
-                  ),
-                ],
+          ),
+          if (_isLoading)
+            Container(
+              color: Colors.black.withOpacity(0.3),
+              child: const Center(
+                child: CircularProgressIndicator(),
               ),
             ),
-            const SizedBox(height: 30),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: colorScheme.primary,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  padding: const EdgeInsets.symmetric(vertical: 15),
-                ),
-                onPressed: () {
-                  context.goNamed('Menu');
-                },
-                child: const Text("Crear",
-                    style: TextStyle(fontSize: 16, color: Colors.white)),
-              ),
-            ),
-            const SizedBox(height: 100),
-          ],
-        ),
+        ],
       ),
     );
   }
-} // Checkpoint
+}
