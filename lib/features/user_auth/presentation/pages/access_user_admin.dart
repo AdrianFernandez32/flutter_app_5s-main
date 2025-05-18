416247880cdf9e7315dd9e825701a8fe886c37d1
AdrianFernandez32 <oscar.adrian.fersa@gmail.com>
2025-05-17 20:28:20 -0700

    fixed nits

--- lib/features/user_auth/presentation/pages/access_user_admin.dart
+++ lib/features/user_auth/presentation/pages/access_user_admin.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import '../widgets/departmentItem.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_app_5s/features/user_auth/presentation/pages/accesses_page.dart';
 
